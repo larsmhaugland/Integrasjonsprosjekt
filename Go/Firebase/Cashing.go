ee0178d4372176e3ac57b22967625d7deda4c45c
Lars Martin Haugland <larmha@stud.ntnu.no>
2023-09-19 17:56:08 +0200

    Lagt inn skall for alle endpoints. Lagt inn autentisering for brukerkredentialer (uten kryptering)

--- Go/Firebase/Cashing.go
+++ Go/Firebase/Cashing.go
@@ -1,1 +1,28 @@
 package Firebase
+
+import "time"
+
+var UserCache map[string]CacheData
+
+func InitCache() {
+	UserCache = make(map[string]CacheData)
+
+}
+
+func GetCacheData(cache map[string]CacheData, key string) (CacheData, bool) {
+	val, ok := cache[key]
+	return val, ok
+}
+
+func ReturnCacheUser(userID string) (User, error) {
+	user, ok := GetCacheData(UserCache, userID)
+	if ok {
+		return user.Data.(User), nil
+	}
+	retUser, err := GetUserData(userID)
+	if err != nil {
+		return User{}, err
+	}
+	UserCache[userID] = CacheData{retUser, time.Now()}
+	return retUser, nil
+}
--- Go/Firebase/Firebase.go
+++ Go/Firebase/Firebase.go
@@ -4,12 +4,13 @@ import (
 	"cloud.google.com/go/firestore"
 	"context"
 	firebase "firebase.google.com/go"
+	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 	"log"
 )
 
 func GetFirestoreClient(ctx context.Context) (*firestore.Client, error) {
-	opt := option.WithCredentialsFile("Database/service-account.json")
+	opt := option.WithCredentialsFile("Firebase/service-account.json")
 	app, err := firebase.NewApp(ctx, nil, opt)
 	if err != nil {
 		log.Printf("error initializing app: %v", err)
@@ -22,4 +23,32 @@ func GetFirestoreClient(ctx context.Context) (*firestore.Client, error) {
 	}
 
 	return client, nil
+}
+
+func GetUserData(userID string) (User, error) {
+	ctx := context.Background()
+	client, err := GetFirestoreClient(ctx)
+	if err != nil {
+		log.Println("error getting Firebase client:", err)
+		return User{}, err
+	}
+	var user User
+	iter := client.Collection("users").Where("username", "==", userID).Documents(ctx)
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			log.Println("Error; No user found")
+			return User{}, err
+		}
+		if err != nil {
+			log.Println("Error; Failed to iterate")
+			return User{}, err
+		}
+		err = doc.DataTo(&user)
+		if err != nil {
+			log.Println("Error converting document:", err)
+			return User{}, err
+		}
+		return user, nil
+	}
 }
