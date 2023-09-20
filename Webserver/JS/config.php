30a4f52f7e99b81c0c5db396a410ee25d4fc2c07
Lars Martin Haugland <larmha@stud.ntnu.no>
2023-09-20 21:11:56 +0200

    jeg hater javascript

--- Webserver/JS/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Content-Type: application/javascript");
-
-// Read the environment variable from Apache (assuming it's set there)
-$apiUrl = getenv("API_URL");
-
-// Generate JavaScript code with the injected variable
-echo "const config = { apiUrl: '$apiUrl' };\n";
-?>
