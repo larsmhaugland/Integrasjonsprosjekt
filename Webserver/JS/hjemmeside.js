9d2f68631245d0d3761b248cd0cd827cca9b217e
seigmann <aurora@skomsvold.net>
2023-09-21 11:42:34 +0200

    Få opp grupper på nettsiden (kun frontend)

--- Webserver/JS/hjemmeside.js
+++ Webserver/JS/hjemmeside.js
@@ -13,6 +13,19 @@ btn = document.querySelector("#close-btn");
 btn.addEventListener("click", (event)=> {event.preventDefault();
     document.getElementById("new-group-popup").style.display = "none";});
 
-//ON SUBMIT, ADD NEW GROUP TO PAGE
+
+//ON SUBMIT CREATE NEW GROUP AND GENERATE GROUP ID
+btn = document.querySelector("#submit-group-btn");
 btn.addEventListener("click",(event)=> {event.preventDefault();
-    /*TODO*/})
+    document.getElementById("new-group-popup").style.display = "none";
+    const groupName = document.querySelector("#gruppenavn").value;
+    newGroup(groupName);});
+
+function newGroup(groupName){
+    let display = document.querySelector(".groups-container");
+
+    let groupBlock = document.createElement("div");
+    groupBlock.setAttribute("id","group-block");
+    groupBlock.textContent = groupName;
+    display.appendChild(groupBlock);
+};
