6bc7f5e741f92437d028fb405532fd8e96770b2b
seigmann <aurora@skomsvold.net>
2023-09-22 11:24:15 +0200

    småkommentering

--- Webserver/JS/hjemmeside.js
+++ Webserver/JS/hjemmeside.js
@@ -1,10 +1,11 @@
 /*
     RETRIEVE GROUPS FROM DATABASE AND DISPLAY
 */
-//retrieveAndDisplayGroups(); COMMENTED OUT BECAUSE THE FUNCTION ISN'T FINISHED YET
+retrieveAndDisplayGroups(); 
 function retrieveAndDisplayGroups(){
     //TODO: Confirm that user is logged in
     //Might have to do another call to get group name and not group id but i am very bad at API and golang and tired
+    //TODO: Save groups to storage session so we don't have to retrieve them every time we refresh the page
     let userName = sessionStorage.getItem("username");
     let credentials = {"username": userName};
     fetch(API_IP + "/user/groups", {
@@ -94,7 +95,7 @@ function newGroup(groupName){
     let groupBlock = document.createElement("div");
     groupBlock.setAttribute("id","group-block");
     groupBlock.textContent = groupName;
-    display.appendChild(groupBlock);        //TODO: Delete this append afterwards (maybe in submitConfirm?) so that we won't get every group id on the page for every new group created
+    display.appendChild(groupBlock);       
 }
 
 /*
@@ -107,8 +108,8 @@ function submitConfirm(){
     const groupName = document.querySelector("#gruppenavn").value;
     let accessCode = document.createElement("p");
     accessCode.setAttribute("id","access-code");
-    accessCode.textContent = newGroup(groupName);
-    display.appendChild(accessCode);
+    accessCode.textContent = newGroup(groupName);   //TODO: Actually post the group id as well
+    display.appendChild(accessCode);    //TODO: Delete this append afterwards (maybe in submitConfirm?) so that we won't get every group id on the page for every new group created
     document.getElementById("group-created-popup").style.display = "block";
 }
 btn = document.querySelector("#close-popup-btn");
