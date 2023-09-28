158efeb22a16156d598a97b63d69eb245ae8e17a
Lars Martin Haugland <larmha@stud.ntnu.no>
2023-09-28 13:48:06 +0200

    php gir meg smerter

--- Webserver/PHP/uploadImage.php
+++ Webserver/PHP/uploadImage.php
@@ -1,10 +1,15 @@
 <?php
 $target_dir = "/usr/local/apache2/images";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_dir . basename($_FILES["recipe_image"]["name"]);
 
-if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
-} else {
-    echo "Sorry, there was an error uploading your file.";
-}
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // The request is using the POST method
+    if(move_uploaded_files($_FILES["recipe_image"]["tmp_name"], $target_file)){
+        echo "The file ". basename( $_FILES["recipe_image"]["name"]). " has been uploaded.";
+    }else{
+        echo "Sorry, there was an error uploading your file.";
+    }
+};
 ?>
