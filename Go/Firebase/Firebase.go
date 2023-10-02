b71fe1588b125854cb70cb5fc607e9127a59afe5
Lars Martin Haugland <larmha@stud.ntnu.no>
2023-10-02 14:56:35 +0200

    trying to fix error with HTTPS cert trying to connect to Firebase v4

--- Go/Firebase/Firebase.go
+++ Go/Firebase/Firebase.go
@@ -22,7 +22,7 @@ var ErrUserExists = errors.New("No user found")
 func GetFirestoreClient(ctx context.Context) (*firestore.Client, error) {
 	// Load the custom CA certificate file
 	customCACertPool := x509.NewCertPool()
-	customCACertPEM, err := ioutil.ReadFile("HTTPS/client.crt") // Replace with the path to your CA certificate
+	customCACertPEM, err := ioutil.ReadFile("HTTPS/client.key") // Replace with the path to your CA certificate
 	if err != nil {
 		log.Printf("Failed to read custom CA certificate: %v", err)
 		return nil, err
